08cc6048e7dff556fdaf452a30ebc0ba053dabf6
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-02 13:02:09 -0500

    Create Monto.js

---
+++ src/Views/Monto.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ModMonto from '../Componentes/ModMonto'
+import Barra from '../Componentes/Barra'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Monto extends Component {
+    render() {
+        return (
+            <div >
+                <div className='App-header'>
+                    <h1> Cambiar monto de estudiante</h1>
+                    <ModMonto />
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Monto
