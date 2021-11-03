7ed6384dfb3fce2c2adb89c48d1d02deb10ceb69
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-03 17:18:32 -0500

    Update Formulario.js

--- src/Componentes/Formulario.js
+++ src/Componentes/Formulario.js
@@ -104,7 +104,7 @@ class Formulario extends Component {
              "Access-Control-Allow-Origin": "*",
              "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
              "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
-           }).then((res) => {
+           }}).then((res) => {
       const {
         isNombre1,
         isNombre2,
