250c5f1bb9697483ea2a0ad9c0126b9509207076
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-04 00:00:49 -0500

    Update Monto.js

--- src/Views/Monto.js
+++ src/Views/Monto.js
@@ -7,7 +7,7 @@ class Monto extends Component {
         return (
             <div >
                 <div className='App-header'>
-                    <h1> Cambiar monto de estudiante</h1>
+                    <h1> Cambiar monto de estudiantessss</h1>
                     <ModMonto />
                 </div>
             </div>
