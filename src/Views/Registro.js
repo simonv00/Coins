f3d8733b589dc708ba4c9740fff0e6602086a5e5
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-03 23:40:05 -0500

    Update Registro.js

--- src/Views/Registro.js
+++ src/Views/Registro.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Formulario from '../Componentes/Formulario'
-import Barra from '../Componentes/Barra'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class Registro extends Component {
